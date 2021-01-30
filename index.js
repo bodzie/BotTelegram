16227b151ab0ca0a76a5ac5504cdd7a895f571ae
Bhdn <novikovbohdan@gmail.com>
2021-01-30 11:50:59 +0100

    init bot

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('hello');
+// console.log(require('telegraf'))
+
+const {Telegraf} = require('telegraf')
+
+const botToken = '1286242213:AAHRRC3Qy15ktw4FYzJs7ytIy0yQuYIHjVw'
+const bot = new Telegraf(botToken)
+
+bot.telegram.getMe().then(console.log)
+
+bot.start((ctx) => ctx.reply('hey there'))
+
+bot.launch()
