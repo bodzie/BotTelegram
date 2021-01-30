c22d70e87e4080a146acfaab8a16a2cb6f6421c0
Bhdn <novikovbohdan@gmail.com>
2021-01-30 21:58:58 +0100

    add new bot token

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ console.log('hello');
 
 const {Telegraf} = require('telegraf')
 
-const botToken = '1286242213:AAHRRC3Qy15ktw4FYzJs7ytIy0yQuYIHjVw'
+const botToken = '1535989556:AAEZRrvit0NvnW-hYieGf59JTYyPg4Q7D8A'
+
 const bot = new Telegraf(botToken)
 
 bot.telegram.getMe().then(console.log)
 
 bot.start((ctx) => ctx.reply('hey there'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
 bot.launch()
